fa1e20ed46061caa85112bd1f50cdd0d22d04ce4
Ivy B <80648381+ivybergstrom@users.noreply.github.com>
2021-09-22 09:11:09 -0600

    Completed phase one, has errors to troubleshoot

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//UL that displays letters guessed
+const guessedLetters = document.querySelector("ul");
+//guess button
+const guessButton = document.querySelector(".guess button");
+//letter input field
+const input = document.querySelector("input");
+//empty paragraph where word in progress will appear
+const progress = document.querySelector(".word-in-progress p");
+//paragraph for remaining guesses in play
+const remain = document.querySelector(".remaining p");
+//span inside the .remaining class, where remaining guesses will display
+const span = document.querySelector("span");
+//empty paragraph that will display messages that encourage the player to guess
+const message = document.querySelector(".message p");
+//play again button, initially hidden
+const playButton = document.querySelector(".play-again button");
+//test word
+const word = "magnolia";
+
+//function to hold circle symbols in place of letters
+const circle = function (word){
+  const placeHolderLetters = [];
+  for (const letter of word) {
+    console.log(letter);
+    placeHolderLetters.push("●");
+  }
+  progress.innerText = placeHolderLetters.join("  ");
+};
+circle(word);
+
+guessButton.addEventListener = function (e) {
+  e.preventDefault();
+  const entryLetter = input.value;
+  console.log(entryLetter);
+  input.value = " ";
+};
